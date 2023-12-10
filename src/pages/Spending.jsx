21800a4a3c538cd197f643e8f890803ec4af5e47
hmyng <uonghongminh07@gmail.com>
2023-12-10 23:11:56 +0700

    spendings data

--- src/pages/Spending.jsx
+++ src/pages/Spending.jsx
@@ -1,54 +1,76 @@
 import { Container } from "@mui/material";
-import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
-import { useState } from "react";
+// import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Search } from "@mui/icons-material";
+import axios from "axios";
 
 function Spending() {
   const [startDate, setStartDate] = useState();
   const [endDate, setEndDate] = useState();
   const today = new Date();
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/spendings')
+      .then((res) => {
+        setData(res.data.data.spendings)
+      })
+  })
+
   return (
     <Container className="spending">
-      <div>
-        <DatePicker
-          className="spending-date-picker"
-          placeholderText="Từ ngày"
-          selected={startDate}
-          onChange={(date) => setStartDate(date)}
-          startDate={startDate}
-          maxDate={today}
-        />
-        <DatePicker
-          className="spending-date-picker"
-          placeholderText="Đến ngày"
-          selected={endDate}
-          onChange={(date) => setEndDate(date)}
-          endDate={endDate}
-          startDate={startDate}
-          minDate={startDate}
-          maxDate={today}
-        />
+      <div className="spending-filter">
+        <div>
+          <DatePicker
+            className="spending-date-picker"
+            placeholderText="Từ ngày"
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            startDate={startDate}
+            maxDate={today}
+          />
+          <DatePicker
+            className="spending-date-picker"
+            placeholderText="Đến ngày"
+            selected={endDate}
+            onChange={(date) => setEndDate(date)}
+            endDate={endDate}
+            startDate={startDate}
+            minDate={startDate}
+            maxDate={today}
+          />
+        </div>
+        <FilterListIcon/>
       </div>
       <div className="spending-search">
-        <Search style={{ color: '#fd7da9' }}/>
+        <Search style={{ color: '#fd7da9', marginRight: '10px' }}/>
         <input className="spending-search-bar" placeholder="Tìm kiếm" />
       </div>
       <div className="spending-list">
-        <div className="spending-date">Today</div>
-        <div className="spending-detail-list">
-          <div className="spending-category">
-            <div className="spending-icon">
-              <BusinessCenterIcon style={{ color: 'red' }} />
-            </div>
-            <div className="spending-info">
-              <div className="spending-info-category">Shopping</div>
-              <div className="spending-info-note">Cloths</div>
-            </div>
+        {data.map((data) => (
+          <div>
+            <div className="spending-date">{data.date}</div>
+            {data.spendings.map((spending) => (
+              <div className="spending-detail-list">
+                <div className="spending-category">
+                  <div className="spending-icon">
+                    {/* <BusinessCenterIcon style={{ color: 'red' }} /> */}
+                    <img src={"http://localhost:3001/icons/"+spending.Category.Icon.content} alt="icon"/>
+                  </div>
+                  <div className="spending-info">
+                    <div className="spending-info-category">{spending.Category.name}</div>
+                    <div className="spending-info-note">{spending.note}</div>
+                  </div>
+                </div>
+                <div className="price">-{spending.money} đ</div>
+              </div>
+            ))}
           </div>
-          <div className="price">-120.000</div>
-        </div>
+        ))}
       </div>
     </Container>
   );
