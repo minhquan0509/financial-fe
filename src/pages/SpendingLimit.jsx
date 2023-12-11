218906b8131fd7702d691f91f92aa31f82a66456
hmyng <uonghongminh07@gmail.com>
2023-12-11 11:07:16 +0700

    spending limit

--- src/pages/SpendingLimit.jsx
+++ src/pages/SpendingLimit.jsx
@@ -1,13 +1,26 @@
 import { Container } from "@mui/material";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import AddIcon from "@mui/icons-material/Add";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { Link } from "react-router-dom";
+import axios from "axios";
+
 function SpendingLimit() {
   const [startDate, setStartDate] = useState();
   const [endDate, setEndDate] = useState();
+  const today = new Date();
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/limits')
+      .then((res) => {
+        setData(res.data.data.limitCategories)
+      })
+  }, [])
   return (
     <Container className="spending">
       <div>
@@ -17,10 +30,40 @@ function SpendingLimit() {
         </Container>
       </div>
       <div className="spending-search">
-        <input className="spending-search-bar" placeholder="Tìm kiếm" />
+        <div className="limit-search">
+          <input className="spending-search-bar" placeholder="Tìm kiếm" />
+        <DatePicker
+            dateFormat="MM/yyyy"
+            className="limit-date-picker"
+            // placeholderText={today}
+            selected={today}
+            onChange={(date) => setStartDate(date)}
+            startDate={startDate}
+            maxDate={today}
+          />
+        </div>
         <Link to="/spendings-limit-add">
           <AddIcon className="spending-limit-add_icon" />
         </Link>
+      </div>
+      <div className="spending-list">
+      {data.map((spending) => (
+              <div className="spending-detail-list">
+                <div className="spending-category">
+                  <div className="spending-icon">
+                    {/* <BusinessCenterIcon style={{ color: 'red' }} /> */}
+                    <img src={"http://localhost:3001/icons/"+spending.Category.Icon.content} alt="icon"/>
+                  </div>
+                  <div className="spending-info">
+                    <div className="spending-info-category">{spending.Category.name}</div>
+                  </div>
+                </div>
+                <div>
+                  <div className="price">{spending.limit_money} đ</div>
+                  <div style={{marginTop: '5px', color: 'gray', fontWeight: '600'}}>{spending.date}</div>
+                </div>
+              </div>
+            ))}
       </div>
     </Container>
   );
