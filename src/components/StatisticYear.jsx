a307842a857b13addbff837fa572a08c332e1de4
minhquan0509 <quandm@caerux.com>
2023-12-17 15:57:34 +0700

    Statistic page

---
+++ src/components/StatisticYear.jsx
@@ -0,0 +1,154 @@
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { BarChart } from "@mui/x-charts/BarChart";
+import { Container } from "@mui/material";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar, getElementAtEvent } from "react-chartjs-2";
+import { useRef } from "react";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+  barThickness: 40,
+  scales: {
+    x: {
+      grid: {
+        display: false,
+      },
+    },
+    y: {
+      grid: {
+        display: false,
+      },
+      ticks: {
+        display: false,
+        beginAtZero: true,
+      },
+    },
+  },
+};
+
+function StatisticYear() {
+  const chartRef = useRef();
+  const [detail, setDetail] = useState(0);
+  const onClick = (event) => {
+    if (getElementAtEvent(chartRef.current, event).length) {
+      setDetail(getElementAtEvent(chartRef.current, event)[0].index);
+    }
+  };
+
+  const [chooseDate, setChooseDate] = useState(new Date());
+  const handleIncrementYear = () => {
+    const newDate = new Date(chooseDate);
+    newDate.setFullYear(chooseDate.getFullYear() + 1);
+    setChooseDate(newDate);
+  };
+  const handleDecrementYear = () => {
+    const newDate = new Date(chooseDate);
+    newDate.setFullYear(chooseDate.getFullYear() - 1);
+    setChooseDate(newDate);
+  };
+  const [dataArray, setDataArray] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        `${
+          process.env.REACT_APP_API_ENDPOINT_PRODUCT
+        }/spendings/statistics?year=${chooseDate.getFullYear()}`
+      )
+      .then((res) => {
+        setDataArray(res.data.data.resultArray);
+      });
+  }, [chooseDate]);
+
+  const getSpendings = () => {
+    if (dataArray.length) {
+      return dataArray.map((item, index) => item.totalSpendings);
+    }
+    return [];
+  };
+  const getDays = () => {
+    if (dataArray.length) {
+      return dataArray.map((item) => `Tháng ${item.month}`);
+    }
+    return [];
+  };
+  const data = {
+    labels: getDays(),
+    datasets: [
+      {
+        data: getSpendings(),
+        backgroundColor: "#74C0FF",
+        barPercentage: 0.9,
+        categoryPercentage: 1,
+      },
+    ],
+  };
+  return (
+    <>
+      <div className="statistic-button-wrapper statistic-button-month-wrapper">
+        <div className="statistic-button-month button-month-back">
+          <ArrowBackIosIcon onClick={handleDecrementYear} />
+        </div>
+        <div className="statistic-month">{chooseDate.getFullYear()}</div>
+        <div className="statistic-button-month button-month-back">
+          <ArrowForwardIosIcon onClick={handleIncrementYear} />
+        </div>
+      </div>
+      <Container className="chart-container">
+        <div className="chart-wrapper">
+          <Bar onClick={onClick} options={options} data={data} ref={chartRef} />
+        </div>
+      </Container>
+      <Container>
+        <table className="statistic-spendings">
+          <thead>
+            {dataArray.length && dataArray[detail] ? (
+              <tr>
+                <th>Tổng cộng:</th>
+                <th>{dataArray[detail].totalSpendings}</th>
+              </tr>
+            ) : (
+              "Không có dữ liệu"
+            )}
+          </thead>
+          <tbody>
+            {dataArray.length
+              ? dataArray[detail].categories.map((item) => (
+                  <tr>
+                    <td>{item.Category.name}:</td>
+                    <td>{item.totalSpendings}</td>
+                  </tr>
+                ))
+              : null}
+          </tbody>
+        </table>
+      </Container>
+    </>
+  );
+}
+
+export default StatisticYear;
--- src/pages/Statistic.jsx
+++ src/pages/Statistic.jsx
@@ -1,14 +1,11 @@
 import { Container } from "@mui/material";
 import Footer from "../components/Footer";
-import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIosNew";
-import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import { BarChart } from "@mui/x-charts/BarChart";
-
-const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
-const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
-const xLabels = ["1", "2", "3", "4", "5", "6", "7"];
+import { useState } from "react";
+import StatisticMonth from "../components/StatisticMonth";
+import StatisticYear from "../components/StatisticYear";
 
 function Statistic() {
+  const [tab, setTab] = useState(1);
   return (
     <>
       <div className="statistic-container">
@@ -16,61 +13,25 @@ function Statistic() {
           <h2 className="statistic-title">Thống kê chi tiêu</h2>
         </Container>
         <div className="statistic-button-wrapper">
-          <div className="statistic-button button-monthly statistic-button-active">
+          <div
+            onClick={() => setTab(1)}
+            className={`statistic-button button-monthly ${
+              tab === 1 ? `statistic-button-active` : null
+            }`}
+          >
             Hàng ngày
           </div>
-          <div className="statistic-button button-yearly">Hàng tháng</div>
+          <div
+            onClick={() => setTab(2)}
+            className={`statistic-button button-yearly ${
+              tab === 2 ? `statistic-button-active` : null
+            }`}
+          >
+            Hàng tháng
+          </div>
         </div>
 
-        <div className="statistic-button-wrapper statistic-button-month-wrapper">
-          <div className="statistic-button-month button-month-back">
-            <ArrowBackIosIcon />
-          </div>
-          <div className="statistic-month">11/2023</div>
-          <div className="statistic-button-month button-month-back">
-            <ArrowForwardIosIcon />
-          </div>
-        </div>
-        <Container>
-          <BarChart
-            // width={500}
-            height={300}
-            series={[
-              {
-                data: uData,
-                label: "uv",
-                id: "uvId",
-                stack: "total",
-                color: "#74C0FF",
-              },
-            ]}
-            xAxis={[{ data: xLabels, scaleType: "band" }]}
-          />
-        </Container>
-        <Container>
-          <table className="statistic-spendings">
-            <thead>
-              <tr>
-                <th></th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Tổng cộng:</td>
-                <td>300000</td>
-              </tr>
-              <tr>
-                <td>Ăn uống:</td>
-                <td>200000</td>
-              </tr>
-              <tr>
-                <td>Ăn uống:</td>
-                <td>200000</td>
-              </tr>
-            </tbody>
-          </table>
-        </Container>
+        {tab === 1 ? <StatisticMonth /> : <StatisticYear />}
       </div>
       <Footer />
     </>
